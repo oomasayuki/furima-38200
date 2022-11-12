9259e0aadd4891cd95074e720d79a708e19fbf25
oomasayuki <ooishimasa19890328@gmail.com>
2022-11-12 21:31:07 +0900

    商品情報編集機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: [:edit]
-
+  
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -21,15 +22,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+   
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,6 +43,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :detail_category_id, :detail_situation_id,
                                  :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
