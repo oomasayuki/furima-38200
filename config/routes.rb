b9bf322bb7df0c84e407d24c2245826ae6b435b4
oomasayuki <ooishimasa19890328@gmail.com>
2022-11-12 21:55:47 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
 
-  resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
  
