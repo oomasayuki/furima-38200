321f92e462d95a228a277805c2717a66c2879c48
oomasayuki <ooishimasa19890328@gmail.com>
2022-11-20 21:09:42 +0900

    do 追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,7 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :post_code, :delivery_area_id, :municipalities, :house_number, :building, :phone_number, :item_id, :user_id, :token
 
-with_options presence: true
+with_options presence: true do
   validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :municipalities
   validates :house_number
