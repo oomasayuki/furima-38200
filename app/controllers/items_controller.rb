159f9069de0d0d6207dda2b89842a3c77d3ecf13
oomasayuki <ooishimasa19890328@gmail.com>
2022-11-20 19:54:18 +0900

    商品購入機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit ,:destroy]
   before_action :set_item, only: [:edit, :show, :update ,:destroy]
   before_action :move_to_index, only: [:edit ,:destroy]
-  
+  before_action :move_to_top_pege, only: :edit
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -59,4 +59,9 @@ class ItemsController < ApplicationController
       redirect_to action: :index
    end
   end
+  def move_to_top_pege
+    if @item.order.present?
+      redirect_to root_path
+   end
+end
 end
